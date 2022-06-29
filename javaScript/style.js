f1695f9b90d74698d12ac392ca0499cf2f00b771
Vladyslav Marynets <smarynec@gmail.com>
2022-06-29 23:02:41 +0300

    create(not finished)section menu

--- javaScript/style.js
+++ javaScript/style.js
@@ -1,2 +1,20 @@
+const  innerTabs = document.querySelector('.tabs'),
+       outerTabs = document.querySelector('.tabs__container'), 
+       btnMenu = document.querySelectorAll('.tabs__item');
+      
+window.addEventListener('load', overFlowtabs);
+window.addEventListener('resize', overFlowtabs);       
 
+btnMenu.forEach(function (item) {
+    item.addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+});
 
+function overFlowtabs() {
+    if (innerTabs.offSetWidth > outerTabs.offSetWidth) {
+        innerTabs.classList.add('overflow');
+    } else {
+        innerTabs.classList.remove('overflow');
+    }
+};
